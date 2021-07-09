ef91008bd491bb51f915ee689e91626590709bbb
siavava <amittaijoel@outlook.com>
2021-07-09 05:19:58 -0400

    checkpoint 02

--- datatypes/datatypes.hs
+++ datatypes/datatypes.hs
@@ -1,15 +1,28 @@
 module Main where
 
 data Calculation =
-  Add Int Int | Sub Int Int | Mul Int Int | Div Int Int | Sum [Int] Int
+  Add Int Int | Sub Int Int | Mul Int Int | Div Int Int | Sum [Int]
+
+
+
 calc :: Calculation -> Int
-calc $ Add x y = x + y
-calc $ Sub x y = x - y
-calc $ Mul x y = x * y
-calc $ Div x y = x / y
-calc $ Sum (x:xs) = x + Sum xs
+calc (x `Add` y) = x + y
+calc (x `Sub` y) = x - y
+calc (x `Mul` y) = x * y
+calc (x `Div` y) = x `div` y
+calc (Sum arr) = Main.sum arr
+
+sum :: [Int] -> Int
+sum = foldr (+) 0
 
 main :: IO ()
 main = do
-  let newCal  = Add 5 42
-  print newCal
+  let newCal  = 5 `Add` 42
+  print $ calc newCal
+
+  let newCal = 27 `Div` 7
+  print $ calc newCal
+
+  let myArr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  let newCalc = Sum myArr
+  print $ calc newCalc
